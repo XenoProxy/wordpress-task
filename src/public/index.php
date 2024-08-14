bd52f86a0506e33f18e5a800b59129c0975abc2a
XenoProxy <xenoproxy@yandex.by>
2024-08-14 16:42:17 +0300

    moving project files

--- src/public/index.php
+++ src/public/index.php
@@ -1,12 +1,4 @@
 <?php
-    // $connect = mysqli_connect($_ENV["MYSQL_HOST"],$_ENV["MYSQL_USER"],$_ENV["MYSQL_PASSWORD"],$_ENV["MYSQL_DATABASE"]);
-    // if (mysqli_connect_errno()) {
-    //     printf("error: %s\n", mysqli_connect_error());
-    //     exit();
-    // }
-    // mysqli_query($connect, "SET NAMES utf8");
-    // echo "Hello";
-
     /**
      * Front to the WordPress application. This file doesn't do anything, but loads
      * wp-blog-header.php which does and tells WordPress to load the theme.
@@ -22,6 +14,6 @@
     define( 'WP_USE_THEMES', true );
 
     /** Loads the WordPress Environment and Template */
-    require __DIR__ . '/../wp-blog-header.php';
+    require __DIR__ . '/wp-blog-header.php';
 
 ?>
