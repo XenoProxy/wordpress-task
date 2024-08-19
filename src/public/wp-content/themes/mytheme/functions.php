cd84a9614835103541654e61ae26c4ccfd8608fb
XenoProxy <xenoproxy@yandex.by>
2024-08-19 14:58:04 +0300

    included custom stylesheet and sample font style

--- src/public/wp-content/themes/mytheme/functions.php
+++ src/public/wp-content/themes/mytheme/functions.php
@@ -7,5 +7,14 @@ function register_my_menus() {
 wp_nav_menu([
   'theme_location'  => 'header_menu'
 ]);
+ 
+function my_theme_style() {
+  wp_enqueue_style( 
+    'my_theme', 
+    get_stylesheet_directory_uri() . '/style.css',     
+    array()
+  );
+}
 
-// add_action( 'init', 'register_my_menus' );
+add_action('wp_enqueue_scripts', 'my_theme_style');
+
