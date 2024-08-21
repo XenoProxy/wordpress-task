17c0cd22afa83b06b6efc258110c26a89e449187
XenoProxy <xenoproxy@yandex.by>
2024-08-21 20:51:24 +0300

    product info displaying on the archive page

--- src/public/wp-content/themes/mytheme/archive-product.php
+++ src/public/wp-content/themes/mytheme/archive-product.php
@@ -1,13 +1,22 @@
-<?php get_header(); ?>
+<?php
+    /* Template Name: Product Archive */
+    get_header(); 
+?>
+
+<?php get_template_part( 'content', 'archive-product' ); ?>
 
 <h2><?php the_archive_title(); ?></h2>
 
-<?php if (have_posts()): while (have_posts()): the_post(); ?>
-    <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-    <div><?php the_post_thumbnail(); ?></div>
-    <div><?php the_excerpt(); ?></div>    
-<?php endwhile;	endif; ?>
+<div id="container">
 
-<?php the_posts_pagination(); ?>
+    <?php if (have_posts()): while (have_posts()): the_post(); ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <h1><?php the_title(); ?></h1>
+        <div><?php the_post_thumbnail(); ?></div>
+        <div><?php the_excerpt(); ?></div>    
+        <a href="<?php the_permalink(); ?>" >Buy</a>
+    <?php endwhile;	endif; ?>
+
+</div>
 
 <?php get_footer(); ?>
