e7fabf105a6cd5f5b2314e97a400f39b77e4a878
XenoProxy <xenoproxy@yandex.by>
2024-08-21 12:29:05 +0300

    while and if cicles formatted

--- src/public/wp-content/themes/mytheme/page.php
+++ src/public/wp-content/themes/mytheme/page.php
@@ -7,9 +7,8 @@
 
 	<section>
 		<?php if (have_posts()): while (have_posts()): the_post(); ?>
-				<?php the_content(); ?>
-		<?php endwhile;
-		endif; ?>
+			<?php the_content(); ?>
+		<?php endwhile;	endif; ?>
 	</section>
 </div>
 
