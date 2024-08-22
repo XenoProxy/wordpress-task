8278e651428f0b26296ea87ca33e39ded613e7d3
XenoProxy <xenoproxy@yandex.by>
2024-08-22 12:51:44 +0300

    archive page for the custom post type fix and image displaying

--- src/public/wp-content/themes/mytheme/archive-product.php
+++ src/public/wp-content/themes/mytheme/archive-product.php
@@ -1,29 +1,27 @@
 <?php
-    /* Template Name: Product Archive */
     get_header(); 
 ?>
 
 <?php get_template_part( 'content', 'archive-product' ); ?>
-
-<?php
-$args = array(
-	'post_type' => 'product',
-	'posts_per_archive_page' => 4
-);
-query_posts( $args );
-?>
 
 <div id="container">
 
     <?php if (have_posts()): while (have_posts()): the_post(); ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         <h1><?php the_title(); ?></h1>
-        <div><?php the_post_thumbnail(); ?></div>
+        <img src="<?php echo get_field('image'); ?>" alt="<?php echo $image['alt']; ?>" />
+        <div><?php echo get_field('price'); ?></div>        
         <div><?php the_excerpt(); ?></div>    
         <a href="<?php the_permalink(); ?>" >Buy</a>
     <?php endwhile;	endif; ?>
     <?php wp_reset_query(); ?>
 
 </div>
+
+<?php
+the_posts_pagination( array(
+	'mid_size' => 2,
+) );
+?>
 
 <?php get_footer(); ?>
--- src/public/wp-content/themes/mytheme/single.php
+++ src/public/wp-content/themes/mytheme/single.php
@@ -1,9 +1,10 @@
 <?php get_header(); ?>
 
-<div class="main-heading">
+<div class="post-heading">
 	<h1><?php the_title(); ?></h1>
 </div>
 
+<img src="<?php echo get_field('image'); ?>" alt="<?php echo $image['alt']; ?>" />
 <div><?php the_content(); ?></div>
 
 <?php get_footer(); ?>
