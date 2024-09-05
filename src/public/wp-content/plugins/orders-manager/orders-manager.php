fd13b06f3aab853c940b004b490e8e988e824cbe
XenoProxy <xenoproxy@yandex.by>
2024-09-05 12:02:59 +0300

    plugin deactiovation check, creating order status column

--- src/public/wp-content/plugins/orders-manager/orders-manager.php
+++ src/public/wp-content/plugins/orders-manager/orders-manager.php
@@ -4,18 +4,42 @@
  * Plugin Name: Orders Manager
  */
 
+activate_plugin('orders-manager/orders-manager.php');
+register_activation_hook(__FILE__, 'manage_posts_columns');
 
-register_activation_hook(__FILE__, array('Akismet', 'plugin_activation'));
-register_deactivation_hook(__FILE__, array('Akismet', 'plugin_deactivation'));
+if (!post_type_exists('custom_order')) {
 
-// Проверка, что тип записи Order существует
-if (!post_type_exists('order')) {
-    deactivate_plugins('orders-manager/orders-manager.php');
+    add_action('admin_init', 'plugin_off');
+    function plugin_off()
+    {
+        deactivate_plugins('orders-manager/orders-manager.php');
+    }
+
+    add_action('admin_notices', 'plugin_notification');
+    function plugin_notification()
+    {
+        echo '<div class="updated">Плагин <p><strong>Orders Manager</strong> был отключен, так как тип записи custom_order не существует</p></div>';
+
+        if (isset($_GET['activate'])) {
+            unset($_GET['activate']);
+        }
+    }
 }
 
 // фильтр manage_posts_columns, чтобы добавить новую колонку в таблицу заказов
-// apply_filters()
+function manage_posts_columns()
+{
+    if(!metadata_exists('post', 97, 'order_status')){
+        echo 'Создано метаполе';
+        add_post_meta(97, 'order_status', ['Canceled', 'Pending', 'Completed']);
+    }    
+}
+apply_filters('add_order_status_column', 'manage_posts_columns');
 
 
 // действие manage_posts_custom_column, чтобы отобразить значение мета поля order_status в новой колонке
-// do_action()
+function manage_posts_custom_column()
+{
+    get_post_meta(97, 'order_status', true);
+}
+do_action('display_order_status_column', 'manage_posts_custom_column');
